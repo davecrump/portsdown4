bd56d4946f64fcc560bd50303772ef1611b8f2f5
davecrump <dave.g8gkq@gmail.com>
2018-04-03 18:06:32 +0000

    201802041_WiFi_Enable

--- scripts/utils/uguir.sh
+++ scripts/utils/uguir.sh
@@ -2,6 +2,12 @@
 cd /home/pi/rpidatv/src/gui
 make clean
 make
+if [ $? != "0" ]; then
+  echo
+  echo "failed install"
+  cd /home/pi
+  exit
+fi
 sudo make install
 cd ../
 
@@ -9,5 +15,4 @@ cd /home/pi
 reset
 sudo killall fbcp >/dev/null 2>/dev/null
 fbcp &
-/home/pi/rpidatv/bin/rpidatvgui 1
-
+/home/pi/rpidatv/scripts/scheduler.sh
