6d191536aed63e3e7b519840dfaf8a2fa69dc9ba
davecrump <dave.g8gkq@gmail.com>
2020-05-21 12:40:47 +0100

    202005312 Correct stream viewer audio output

--- scripts/omx_udp.sh
+++ scripts/omx_udp.sh
@@ -3,7 +3,34 @@
 # Called by rpidatvtouch to start the player for IPTS and return stdout
 # in a status file
 
-stdbuf -oL omxplayer --video_queue 0.5 --timeout 5 udp://:@:10000 2>/dev/null | {
+############ FUNCTION TO READ CONFIG FILE #############################
+
+get_config_var() {
+lua - "$1" "$2" <<EOF
+local key=assert(arg[1])
+local fn=assert(arg[2])
+local file=assert(io.open(fn))
+for line in file:lines() do
+local val = line:match("^#?%s*"..key.."=(.*)$")
+if (val ~= nil) then
+print(val)
+break
+end
+end
+EOF
+}
+########################################################################
+
+RCONFIGFILE="/home/pi/rpidatv/scripts/longmynd_config.txt"
+
+# Send audio to the correct port (uses Port selected in RX Config)
+if [ "$AUDIO_OUT" == "rpi" ]; then
+  AUDIO_MODE="local"
+else
+  AUDIO_MODE="alsa:plughw:1,0"
+fi
+
+stdbuf -oL omxplayer --adev $AUDIO_MODE --video_queue 0.5 --timeout 5 udp://:@:10000 2>/dev/null | {
 LINE="1"
 rm  /home/pi/tmp/stream_status.txt >/dev/null 2>/dev/null
 while IFS= read -r line
