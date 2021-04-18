114b4de3a9f78644f12ded2465bf6125662907a8
davecrump <dave.g8gkq@gmail.com>
2021-04-18 17:30:24 +0100

    202104180 Bandview corrections

--- src/bandview/font/font.h
+++ src/bandview/font/font.h
@@ -1,9 +1,11 @@
 #ifndef __FONT_H__
 #define __FONT_H__
 
-#ifndef __SCREEN_H__
-  typedef void screen_pixel_t;
-#endif
+//#ifndef __SCREEN_H__
+//  typedef void screen_pixel_t;
+//#endif
+
+
 
 typedef struct {
     const uint32_t height;
@@ -18,13 +20,21 @@ typedef struct {
     const font_character_t *characters;
 } font_t;
 
-uint32_t font_render_string_with_callback(int x, int y, const font_t *font_ptr, char *string, void (*render_cb)(int x, int y, screen_pixel_t *pixel_ptr));
-uint32_t font_render_colour_string_with_callback(int x, int y, const font_t *font_ptr, const screen_pixel_t *pixel_background_ptr, const screen_pixel_t *pixel_foreground_ptr, char *string, void (*render_cb)(int x, int y, screen_pixel_t *pixel_ptr));
 
-uint32_t font_render_character_with_callback(int x, int y, const font_t *font_ptr, char c, const screen_pixel_t *pixel_background, const screen_pixel_t *pixel_foreground, void (*render_cb)(int x, int y, screen_pixel_t *pixel_ptr));
 
 //uint32_t font_width_string(const font_t *font_ptr, char *string);
+//void displayChar2(const font_t *font_ptr, char*c);
+//void TextMid2 (int xpos, int ypos, char*s, const font_t *font_ptr);
+//void Text2 (int xpos, int ypos, char*s, const font_t *font_ptr);
 
+
+#include "dejavu_sans_18.h"
+#include "dejavu_sans_20.h"
+#include "dejavu_sans_22.h"
+#include "dejavu_sans_24.h"
+#include "dejavu_sans_26.h"
+#include "dejavu_sans_28.h"
+#include "dejavu_sans_30.h"
 #include "dejavu_sans_32.h"
 #include "dejavu_sans_36.h"
 #include "dejavu_sans_72.h"
---
+++ src/bandview/graphics.h
@@ -0,0 +1,11 @@
+#ifndef __GRAPHICS_H__
+#define __GRAPHICS_H__
+
+void waterfall_render_fft(uint8_t *fft_data);
+
+void graphics_frequency_newdata(void);
+void graphics_if_fft_newdata(uint8_t *fft_data);
+
+
+#endif /* __GRAPHICS_H__ */
+
