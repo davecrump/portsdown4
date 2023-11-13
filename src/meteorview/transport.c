2b829f855fe48db10030443bbeae41cc6a839f45
davecrump <dave.g8gkq@gmail.com>
2023-11-13 20:51:04 +0000

    202311130 Adjust MeteorView remote scaling

--- src/meteorview/transport.c
+++ src/meteorview/transport.c
@@ -16,18 +16,22 @@
 
 static int sockfd = 0;
 bool connection_made;
+extern bool app_exit;
 
 void transport_init()
 {
-    int err=0;
-    connection_made = false;
-    struct sockaddr_in serv_addr; 
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return;
-    } 
+  int err = 0;
+  connection_made = false;
+  struct sockaddr_in serv_addr;
 
+  // Create socket
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    printf("\n Error : Could not create socket \n");
+    //return;
+  } 
+  else  // Socket created
+  {
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
     serv_addr.sin_family = AF_INET;
@@ -36,40 +40,73 @@ void transport_init()
     if(inet_pton(AF_INET, "95.154.252.125", &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
-        return;
+        //return;
     } 
-
-    if ((err=connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
+    else   // Server found
     {
-       printf("Error : Connect Failed %i %i ",err,errno);
-       printf("connect: %s\n", strerror(errno));
-       return;
-    } 
+      if ((err=connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
+      {
+        printf("Error : Connect Failed %i %i ",err,errno);
+        printf("connect: %s\n", strerror(errno));
+        //return;
+      } 
+      else            // Connected
+      {
+        /* make it non blocking */
+        fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL) | O_NONBLOCK);
 
-    /* make it non blocking */
-    fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL) | O_NONBLOCK);
-
-    connection_made = true;
-    printf("Connection to server established\n");
+        connection_made = true;
+        printf("Connection to server established\n");
+      }
+    }
+  }
+  
+  // Restart app if no connection and not already exiting
+  if ((connection_made == false) && (app_exit == false))
+  {
+    usleep(1000000);
+    cleanexit(150);
+  }
 }
 
 
-void transport_send(unsigned char *data) {
-    int err=0;
+void transport_send(unsigned char *data)
+{
+  int err = 0;
+  int error_count = 0;
 
 //    if (connection_made) {
 //        write(sockfd, data, TCP_DATA_PRE + FFT_BUFFER_SIZE*sizeof(short)*2);
 //    }
-    do {
-        err = write(sockfd, data, TCP_DATA_PRE + FFT_BUFFER_SIZE*sizeof(short)*2);
-        if (err<0) {
-            printf("Error : Write failed %i %i ",err,errno);
-            printf(" : %s\n", strerror(errno));
-            usleep(10000);
-        }
-    } while ((err<0) && (errno==11));
-    if (err<0) {
-        printf("Reconnecting \n");
-        transport_init();
+
+  do
+  {
+    err = write(sockfd, data, TCP_DATA_PRE + FFT_BUFFER_SIZE * sizeof(short) * 2);
+        
+    if (err < 0)
+    {
+      printf("Error : Write failed %i %i ",err,errno);
+      printf(" : %s\n", strerror(errno));
+      usleep(10000);
+      error_count++;
     }
+    else
+    {
+      error_count = 0;
+    }
+
+    if ((error_count > 10) && (app_exit == false))  // errors and not already exiting
+    {
+      usleep(1000000);
+      cleanexit(150);                  // Restart MeteorViewer after 10 attempts
+    }
+  }
+  while ((err < 0) && (errno == 11));
+
+  if (err < 0)
+  {
+    usleep(1000000);
+    printf("Reconnecting \n");
+    transport_init();
+  }
 }
